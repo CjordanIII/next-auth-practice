cdf0ea19fa0384657f4beafb3af4cbd13bacae61
Clarence Jordan <clarencejordan087@gmail.com>
2024-07-14 00:03:58 -0500

    add register

--- src/components/auth/login/social.tsx
+++ src/components/auth/login/social.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
-import { Button } from "../ui/button";
+import { Button } from "../../ui/button";
 export const Social = () => {
   return (
     <div className="flex items-center w-full gap-x-2">
