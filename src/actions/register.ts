a126a2793f9052718e8d7128334f99037f586885
Clarence Jordan <clarencejordan087@gmail.com>
2024-07-14 23:28:19 -0500

    add next auth upgrade add prisma schema and dev dependencys add db connection add neon db

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,11 +1,33 @@
 "use server";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import bycrypt from "bcrypt";
 import * as z from "zod";
+import { getUserByEmail } from "../../data/user";
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
    
     const validatedFields = RegisterSchema.safeParse(values)
     if (!validatedFields.success) {
         return {error: "Invalid fields!"}
     }
-    return{success:"Email sent!"}
+
+    const { email, password, name } = validatedFields.data
+    const hashedPassword = await bycrypt.hash(password, 10)
+    
+    const existingUser = await getUserByEmail(email)
+    if (existingUser) {
+        return { error: "Email already in use!" }
+        
+    }
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+// TODO: send varification token email
+
+
+    return{success:"User Created"}
 }
